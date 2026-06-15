3bd70f33efe0961f4e64abd5bc46a625417a3657
Jackrichards <jack.richards@cathcollege.school.nz>
2026-06-15 19:56:20 +1200

    tweaked achievements and settings

--- assessment-for-year-12/Scripts/settings.gd
+++ assessment-for-year-12/Scripts/settings.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
