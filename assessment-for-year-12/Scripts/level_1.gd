05006355d7636662666e8d51551f7ab908a7c201
Jackrichards <jack.richards@cathcollege.school.nz>
2026-08-31 09:06:14 +1200

    added enemy skin, acid vats, respawn warning, acid vat tweaks in code

--- assessment-for-year-12/Scripts/level_1.gd
+++ assessment-for-year-12/Scripts/level_1.gd
@@ -8,3 +8,7 @@ func _on_elevator_body_entered(body: Node2D) -> void:
 	if Player and body.battery_count >= 8:
 		print("you won")
 		get_tree().change_scene_to_file("res://Scenes/win_menu.tscn")
+
+
+func _on_acid_vat_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
