9831b196bb61ce9441d3456782f88dd2ad2b7191
Jackrichards <jack.richards@cathcollege.school.nz>
2026-09-13 20:50:17 +1200

    added main menu stuff

--- assessment-for-year-12/Scripts/main_menu.gd
+++ assessment-for-year-12/Scripts/main_menu.gd
@@ -1,15 +1,18 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _ready():
+	main_buttons.visible = true
+	settings.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_menu.tscn")
@@ -20,8 +23,13 @@ func _on_achievements_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+	main_buttons.visible = false
+	settings.visible = true
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
--- assessment-for-year-12/Scripts/settings.gd
+++ assessment-for-year-12/Scripts/settings.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +10,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_button_pressed() -> void:
+func _on_back_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
