c630327bdab19304d90b9abe99d9806ec766d3fe
Jack Richards <jack.richards@cathcollege.school.nz>
2026-09-17 13:30:04 +1200

    Assets from josh, level 1 fix, settings button on level 1.
    The code changes is because I was attempting to attach my settings to the setting button so I could open it across levels.

--- assessment-for-year-12/Scripts/level_1.gd
+++ assessment-for-year-12/Scripts/level_1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var settings: Panel = $Settings
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
@@ -8,3 +10,7 @@ func _on_elevator_body_entered(body: Node2D) -> void:
 	if Player and body.battery_count >= 8:
 		print("you won")
 		get_tree().change_scene_to_file("res://Scenes/win_menu.tscn")
+
+
+func _on_settings_pressed() -> void:
+	settings.visible = true
