3471a19dab82d9ad27dd4f19db095ba884dba27a
Jackrichards <jack.richards@cathcollege.school.nz>
2026-09-21 09:19:07 +1200

    Set up level 2 and 3 and rearanged some signals to be consistent across all levels

--- assessment-for-year-12/Scripts/level_menu.gd
+++ assessment-for-year-12/Scripts/level_menu.gd
@@ -8,5 +8,12 @@ func _on_level_1_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
+func _on_level_2_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+	
+func _on_level_3_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
+	
+
 func _on_back_to_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
