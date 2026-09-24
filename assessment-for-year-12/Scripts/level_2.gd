deaff7a2439c15cace650a5308904a9a1d9f0558
Jackrichards <jack.richards@cathcollege.school.nz>
2026-09-24 13:21:11 +1200

    Fixed the issue of battery count and player not being called on level 1 and 2, also added the achievement assets into the assets folder

--- assessment-for-year-12/Scripts/level_2.gd
+++ assessment-for-year-12/Scripts/level_2.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 
-@onready var settings: Panel = $Settings
+@onready var player: Player = $Player
 
 
 func _on_back_to_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
 func _on_elevator_body_entered(body: Node2D) -> void:
-	if Player and body.battery_count >= 10:
+	if player.battery_count >= 10:
 		print("you won")
 		get_tree().change_scene_to_file("res://Scenes/win_menu.tscn")
